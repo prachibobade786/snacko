15e1c978132343453d2958579c3986487f56a3a8
Sanika <sanikadeshpande15@gmail.com>
2026-07-23 14:30:20 +0530

    userroutes

--- backend/src/modules/user/userroutes.js
+++ backend/src/modules/user/userroutes.js
@@ -8,9 +8,10 @@ const verifyToken = require("../../middleware/authmiddleware");
 router.post("/register", userController.register);
 
 router.post("/login", userController.login);
+router.post("/forgot-password", userController.forgotPassword);
+router.post("/reset-password", userController.resetPassword);
 
 router.get("/profile", verifyToken, userController.getProfile);
-
-
+router.put("/profile", verifyToken, userController.updateProfile);
 
 module.exports = router;
